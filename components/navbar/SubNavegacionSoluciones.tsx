317f6a2073e4fcf85a62c3dde3653d390a20d105
Diego Altamirano <diegoalt.g@hotmail.com>
2025-01-30 16:30:42 -0300

    solucion

--- components/navbar/SubNavegacionSoluciones.tsx
+++ components/navbar/SubNavegacionSoluciones.tsx
@@ -63,23 +63,23 @@ export const SubNavegacionSoluciones:React.FC<Props> = ({setHoverSubSoluciones,h
           <div className={` text-white md:ml-10 lg:ml-[310px]  xl:ml-[368px] mr-96 ${hoverSubSoluciones ? 'flex' : 'hidden'}`} onMouseLeave={()=>outHover()} >
               <div className="z-30 bg-[#1E1E1E] fixed top-[63px] ">
                 
-                <a href="https://ecoalliance.cl/camion-multiproposito/" className="text-white text-xs py-4 flex border-b border-slate-600 pl-10 pr-5 " onMouseEnter={()=>hoverNav()}>SOLUCIONES MULTIPROPÓSITO</a>
+                <a href="https://ecoalliance.cl/camion-multiproposito/" className="text-white text-xs py-4 flex border-b border-slate-600 pl-[55px] pr-5 " onMouseEnter={()=>hoverNav()}>SOLUCIONES MULTIPROPÓSITO</a>
                 
-                <a  onMouseEnter={()=>hoverNav()} href="https://ecoalliance.cl/soluciones-tratamiento-de-residuos/" className="text-white text-xs py-4 flex border-b border-slate-600 pl-10 pr-5"  >TRATAMIENTO DE RESIDUOS</a>
+                <a  onMouseEnter={()=>hoverNav()} href="https://ecoalliance.cl/soluciones-tratamiento-de-residuos/" className="text-white text-xs py-4 flex border-b border-slate-600 pl-[55px] pr-5"  >TRATAMIENTO DE RESIDUOS</a>
                 
                 <div className="flex" >
-                  <div className="bg-[#1E1E1E] w-[10px]" onMouseEnter={()=>hoverBonus()}></div>
+                  <div className="bg-[#1E1E1E] w-[15px]" onMouseEnter={()=>hoverBonus()}></div> 
                   <a onMouseEnter={()=>onHoverSolucionAseo()}  href="https://ecoalliance.cl/soluciones-aseo-urbano/" className="text-white text-xs py-4 flex border-b border-slate-600 pl-10 pr-5 w-full">ASEO URBANO<IoMdArrowDropright className="text-lg"/></a>
 
                 </div>
                 
                 <div className="flex">
-                  <div className="bg-[#1E1E1E] w-[10px]" onMouseEnter={()=>hoverBonus()}></div>
+                  <div className="bg-[#1E1E1E] w-[15px]" onMouseEnter={()=>hoverBonus()}></div>
                   <a onMouseEnter={()=>onHoverSolucionMantenimiento()} href="https://ecoalliance.cl/soluciones-mantenimiento-urbano/" className="text-white text-xs py-4 flex border-b border-slate-600 pl-10 pr-5">MANTENIMIENTO URBANO<IoMdArrowDropright className="text-lg"/></a>
 
                 </div>
                 
-                <a href="https://ecoalliance.cl/espacios-publicos/" className="text-white text-xs py-4 flex border-b border-slate-600 pl-10 pr-5"  onMouseEnter={()=>hoverNav()}>ESPACIOS PÚBLICOS</a>
+                <a href="https://ecoalliance.cl/espacios-publicos/" className="text-white text-xs py-4 flex border-b border-slate-600 pl-[55px] pr-5"  onMouseEnter={()=>hoverNav()}>ESPACIOS PÚBLICOS</a>
               </div>
           </div>
 
