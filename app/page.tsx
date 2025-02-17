ff2e6e1f0dab0c8db4b5b7f63158783256bcd630
Diego Altamirano <diegoalt.g@hotmail.com>
2025-02-17 11:49:41 -0300

    agregando funcionalidad slider automatico

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import BannerAutomatico from "@/components/bannerAutomatico/BannerAutomatico"
 import dynamic from "next/dynamic";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import BannerPrincipal from "@/components/bannerAutomatico/BannerPrincipal";
 
 const Slider = dynamic(() => import("react-slick"), { ssr: false });
 
@@ -60,15 +61,11 @@ const HomePage = () =>{
     return(
         <main className="mt-36 mx-5 md:mx-16">
             
-            <section className=" bg-gray-200 rounded-3xl py-24 px-16">
-                <h2 className="text-[25px] md:text-3xl font-bold">MAXIMIZA TU EFICIENCIA CON <br/> TECNOLOGIA DE VANGUARDIA</h2>
-                <p className="mt-8 text-2xl hidden md:block">Utiliza maquinaria eficiente para limpieza y residuos <br></br>orgánicos</p>
-                <button className="bg-white text-[16px] py-2 px-7 rounded-lg mt-12 font-bold">Contáctanos</button>
-            </section>
+          
+            <BannerPrincipal/>
 
-
-            <section className="mt-24 mb-24 w-full">
-                <h2 className="text-3xl font-bold">Productos</h2>
+            <section className="mt-24 mb-24 w-full ">
+                <h2 className="text-3xl font-bold text-[#A7283D]">Productos</h2>
                 <div className="lg:hidden ">
 
                     <Slider {...settings} >
@@ -110,88 +107,12 @@ const HomePage = () =>{
 
                 </div>
               
-
-{/* 
-                <div className="flex sm:justify-between sm:flex-wrap ">
-
-                    <div className=" md:w-[48%] lg:w-[32%] xl:w-[24%] rounded-xl shadow-lg shadow-gray-300 mt-14">
-                        <div className="bg-gray-100 h-[169px] rounded-t-xl">
-                        </div>
-                        <div className="px-7 pb-14">
-                            <h3 className="mt-4 text-2xl">Chipeadora</h3>
-                            <p className="mt-7 text-xl">Reduce Residuos de Madera y Restos de Poda. Chipeadoras de Disco, Chipeadora PTO, Chipeadoras de Tambor, Chipeadoras sobre Orugas y Chipeadoras Especiales.</p>
-                        </div>
-                    </div>
-
-                    <div className=" md:w-[48%] lg:w-[32%] xl:w-[24%]  rounded-xl shadow-lg shadow-gray-300 mt-14">
-                        <div className="bg-gray-100 h-[169px] rounded-t-xl">
-                        </div>
-                        <div className="px-7 pb-14">
-                            <h3 className="mt-4 text-2xl">Chipeadora</h3>
-                            <p className="mt-7 text-xl">Reduce Residuos de Madera y Restos de Poda. Chipeadoras de Disco, Chipeadora PTO, Chipeadoras de Tambor, Chipeadoras sobre Orugas y Chipeadoras Especiales.</p>
-                        </div>
-                    </div>
-
-                    <div className="md:w-[48%] lg:w-[32%] xl:w-[24%]  rounded-xl shadow-lg shadow-gray-300 mt-14">
-                        <div className="bg-gray-100 h-[169px] rounded-t-xl">
-                        </div>
-                        <div className="px-7 pb-14">
-                            <h3 className="mt-4 text-2xl">Chipeadora</h3>
-                            <p className="mt-7 text-xl">Reduce Residuos de Madera y Restos de Poda. Chipeadoras de Disco, Chipeadora PTO, Chipeadoras de Tambor, Chipeadoras sobre Orugas y Chipeadoras Especiales.</p>
-                        </div>
-                    </div>
-
-                    <div className="md:w-[48%] lg:w-[32%] xl:w-[24%]  rounded-xl shadow-lg shadow-gray-300 mt-14">
-                        <div className="bg-gray-100 h-[169px] rounded-t-xl">
-                        </div>
-                        <div className="px-7 pb-14">
-                            <h3 className="mt-4 text-2xl">Chipeadora</h3>
-                            <p className="mt-7 text-xl">Reduce Residuos de Madera y Restos de Poda. Chipeadoras de Disco, Chipeadora PTO, Chipeadoras de Tambor, Chipeadoras sobre Orugas y Chipeadoras Especiales.</p>
-                        </div>
-                    </div>
-
-                    <div className="md:w-[48%] lg:w-[32%] xl:w-[24%]  rounded-xl shadow-lg shadow-gray-300 mt-14">
-                        <div className="bg-gray-100 h-[169px] rounded-t-xl">
-                        </div>
-                        <div className="px-7 pb-14">
-                            <h3 className="mt-4 text-2xl">Aseo urbano</h3>
-                            <p className="mt-7 text-xl">Reduce Residuos de Madera y Restos de Poda. Chipeadoras de Disco, Chipeadora PTO, Chipeadoras de Tambor, Chipeadoras sobre Orugas y Chipeadoras Especiales.</p>
-                        </div>
-                    </div>
-
-                    <div className="md:w-[48%] lg:w-[32%] xl:w-[24%]  rounded-xl shadow-lg shadow-gray-300 mt-14">
-                        <div className="bg-gray-100 h-[169px] rounded-t-xl">
-                        </div>
-                        <div className="px-7 pb-14">
-                            <h3 className="mt-4 text-2xl">Espacios públicos</h3>
-                            <p className="mt-7 text-xl">Reduce Residuos de Madera y Restos de Poda. Chipeadoras de Disco, Chipeadora PTO, Chipeadoras de Tambor, Chipeadoras sobre Orugas y Chipeadoras Especiales.</p>
-                        </div>
-                    </div>
-
-                    <div className="md:w-[48%] lg:w-[32%] xl:w-[24%]  rounded-xl shadow-lg shadow-gray-300 mt-14">
-                        <div className="bg-gray-100 h-[169px] rounded-t-xl">
-                        </div>
-                        <div className="px-7 pb-14">
-                            <h3 className="mt-4 text-2xl">Multipropósitos</h3>
-                            <p className="mt-7 text-xl">Reduce Residuos de Madera y Restos de Poda. Chipeadoras de Disco, Chipeadora PTO, Chipeadoras de Tambor, Chipeadoras sobre Orugas y Chipeadoras Especiales.</p>
-                        </div>
-                    </div>
-
-                    <div className="md:w-[48%] lg:w-[32%] xl:w-[24%]  rounded-xl shadow-lg shadow-gray-300 mt-14">
-                        <div className="bg-gray-100 h-[169px] rounded-t-xl">
-                        </div>
-                        <div className="px-7 pb-14">
-                            <h3 className="mt-4 text-2xl">Chipeadora</h3>
-                            <p className="mt-7 text-xl">Reduce Residuos de Madera y Restos de Poda. Chipeadoras de Disco, Chipeadora PTO, Chipeadoras de Tambor, Chipeadoras sobre Orugas y Chipeadoras Especiales.</p>
-                        </div>
-                    </div>
-                </div> */}
             </section>
 
          
 
             <section className="mb-24">
-                <h2 className="text-3xl font-bold">Nuevos productos</h2>
+                <h2 className="text-3xl font-bold text-[#A7283D]">Nuevos productos</h2>
 
                 <BannerAutomatico/>
                 
--- components/bannerAutomatico/BannerAutomatico.tsx
+++ components/bannerAutomatico/BannerAutomatico.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Image from "next/image";
 import { useState, useEffect, useRef } from "react";
 import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
