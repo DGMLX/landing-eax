d30c85058c56c241c91de9baaeab4f4169b08390
Diego Altamirano <diegoalt.g@hotmail.com>
2025-02-19 11:28:48 -0300

    agregando funcionalidad a slider principal

--- components/bannerAutomatico/BannerPrincipal.tsx
+++ components/bannerAutomatico/BannerPrincipal.tsx
@@ -2,8 +2,9 @@
 
 import { useEffect, useRef, useState } from "react";
 import { HiArrowLongRight } from "react-icons/hi2";
-import { IoIosArrowBack } from "react-icons/io";
-import { IoIosArrowForward } from "react-icons/io";
+import { MdArrowBackIos } from "react-icons/md";
+import { MdArrowForwardIos } from "react-icons/md";
+
 
 const banners = [
     {
@@ -63,16 +64,30 @@ const BannerPrincipal:React.FC = () =>{
 
     return(
         <>
-          <section className=" bg-gray-400  py-24 pl-32  h-[407px] lg:[500px]  flex-col justify-center flex">
-              <div className="w-1/2">
-                  <h2 className="text-lg font-bold text-[#FBFBFB] mb-3">{banners[currentBanner].header}</h2>
-                  <h2 className="text-[21px] md:text-3xl font-bold text-[#FBFBFB]">{banners[currentBanner].titulo}</h2>
-                  <p className="mt-3 text-lg hidden md:block text-[#FBFBFB]">{banners[currentBanner].descripcion}</p>
-                  <div>
-                      <button className="hover:bg-[#A7283D] bg-[#e34f4f] text-white text-[16px] py-2 px-7 rounded-full mt-12 font-bold   flex items-center">{banners[currentBanner].boton}<HiArrowLongRight className="text-3xl ml-4"/></button>
+          <section className=" bg-gray-400  pt-24 pb-16 h-[450px]  justify-center flex-col flex">
+            
+    
+
+              <div className="w-1/2  pl-32 ">
+                  <div className="h-[250px] w-full">
+                    <h2 className="text-lg font-bold text-[#FBFBFB] mb-3 ">{banners[currentBanner].header}</h2>
+                    <h2 className="text-[21px] md:text-3xl font-bold text-[#FBFBFB]">{banners[currentBanner].titulo}</h2>
+                    <p className="mt-3 text-lg hidden md:block text-[#FBFBFB]">{banners[currentBanner].descripcion}</p>
+                    <div>
+                        <button className="hover:bg-[#A7283D] bg-[#e34f4f] text-white text-[16px] py-2 px-7 rounded-full mt-12 font-bold   flex items-center">{banners[currentBanner].boton}<HiArrowLongRight className="text-3xl ml-4"/></button>
+                    </div>
                   </div>
               </div>
-            </section>
+              <div className="flex justify-center mt-16 items-center gap-2">
+                <button className={` h-[10px] w-[30px] rounded-full ${currentBanner === 0 ? 'bg-[#c4262e]' : 'bg-white'}`} onClick={()=>setCurrentBanner(0)}></button>
+                <button className={` h-[10px] w-[30px] rounded-full ${currentBanner === 1 ? 'bg-[#c4262e]' : 'bg-white'}`} onClick={()=>setCurrentBanner(1)}></button>
+                <button className={` h-[10px] w-[30px] rounded-full ${currentBanner === 2 ? 'bg-[#c4262e]' : 'bg-white'}`} onClick={()=>setCurrentBanner(2)}></button>
+                <button className={` h-[10px] w-[30px] rounded-full ${currentBanner === 3 ? 'bg-[#c4262e]' : 'bg-white'}`} onClick={()=>setCurrentBanner(3)}></button>
+                <button className={` h-[10px] w-[30px] rounded-full ${currentBanner === 4 ? 'bg-[#c4262e]' : 'bg-white'}`} onClick={()=>setCurrentBanner(4)}></button>
+              </div>
+      
+
+          </section>
         </>
     )
 }
