6cb7d8945186f2c7392d66be655aa30ece20f941
Diego Altamirano <diegoalt.g@hotmail.com>
2025-02-27 18:10:38 -0300

    imagen blog

--- app/inicio/page.tsx
+++ app/inicio/page.tsx
@@ -311,7 +311,7 @@ const HomePage = () =>{
                 <h2 className="text-2xl mb-5 tracking-widest">PERSPECTIVAS</h2>
                 <div className="flex-col md:flex-row  lg:flex-row flex gap-5 mb-7 ">
                     <div className="bg-gray-300 w-full sm:w-full md:w-1/2 h-[350px] flex flex-col justify-center" style={{
-                        backgroundImage:'url(/inicio/blog2.jpg)',
+                        backgroundImage:'url(/inicio/blog.png)',
                         backgroundBlendMode:"darken",
                         backgroundRepeat:"no-repeat",
                         backgroundSize: "cover",
